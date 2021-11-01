d38319847042243e8f8f4429094f75db6400c379
Anran Zheng <85720119+Anran0716@users.noreply.github.com>
2021-11-01 19:09:54 -0400

    Add files via upload

--- 508 HW4.R
+++ 508 HW4.R
@@ -110,12 +110,17 @@ housing.eng<-housing%>%
       spent_on_repairs < 5000 ~ "repair5000-",
       spent_on_repairs >= 5000 & spent_on_repairs < 5100 ~ "repair5000_5100",
       spent_on_repairs >= 5100 & spent_on_repairs < 5200 ~ "repair5100_5200",
-      spent_on_repairs >= 5200 ~ "repair5200+")
+      spent_on_repairs >= 5200 ~ "repair5200+"),
+    season = case_when(
+      month == "mar"| month == "apr"| month == "may" ~ "Spring",
+      month == "jun" | month == "jul" | month == "aug"~ "Summer",
+      month == "sep"| month == "oct" | month == "nov" ~ "Fall",
+      month == "dec" ~ "Winter")
   )
 
 #Interpret your new features
 housing.eng%>%
-  dplyr::select(y, age.cat, previous.cat, price.cat, conf.cat, inflation.cat, repair.cat) %>%
+  dplyr::select(y, age.cat, previous.cat, price.cat, conf.cat, inflation.cat, repair.cat,season) %>%
   gather(Variable, value, -y) %>%
   count(Variable, value, y) %>%
   ggplot(aes(value, n, fill = y)) +   
@@ -160,18 +165,18 @@ kitchenTest<-housingTest%>%
 #engineering
 engTrain<-housingTrain%>%
   dplyr::select(
-    y,y_numeric,age.cat, previous.cat, price.cat, conf.cat, inflation.cat, repair.cat,
+    y,y_numeric,age.cat, previous.cat, price.cat, conf.cat, inflation.cat, 
     job, marital, education,taxLien,
-    mortgage, taxbill_in_phl, contact, month,
-    day_of_week, campaign, pdays, poutcome
+    mortgage, taxbill_in_phl, contact, 
+    day_of_week, campaign, pdays, season, poutcome
   )
 
 engTest<-housingTest%>%
   dplyr::select(
-    y,y_numeric,age.cat, previous.cat, price.cat, conf.cat, inflation.cat, repair.cat,
+    y,y_numeric,age.cat, previous.cat, price.cat, conf.cat, inflation.cat, 
     job, marital, education,taxLien,
-    mortgage, taxbill_in_phl, contact, month,
-    day_of_week, campaign, pdays, poutcome
+    mortgage, taxbill_in_phl, contact, 
+    day_of_week, campaign, pdays, season, poutcome
   )
 
 #kitchen sink regression
@@ -209,20 +214,41 @@ eng.cvFit <- train(y ~ ., data = engTest %>%
 
 eng.cvFit
 
-#Goodness of fit(unsolved)
+#Goodness of fit
 kitchen.testProbs <- data.frame(Outcome = as.factor(kitchenTest$y_numeric),
                         Probs = predict(kitchen.reg, kitchenTest, type= "response"))
+kitchen.testProbs <- 
+  kitchen.testProbs %>%
+  mutate(predOutcome  = as.factor(ifelse(kitchen.testProbs$Probs > 0.5 , 1, 0)))
 
+caret::confusionMatrix(kitchen.testProbs$predOutcome, kitchen.testProbs$Outcome, 
+                       positive = "1")
 
 eng.testProbs <- data.frame(Outcome = as.factor(engTest$y_numeric),
                                 Probs = predict(eng.reg, engTest, type= "response"))
+eng.testProbs <- 
+  eng.testProbs %>%
+  mutate(predOutcome  = as.factor(ifelse(eng.testProbs$Probs > 0.5 , 1, 0)))
 
-#ROC curve(unsolved)
-ggplot(testProbs, aes(d = as.numeric(testProbs$Outcome), m = Probs)) +
+caret::confusionMatrix(eng.testProbs$predOutcome, eng.testProbs$Outcome, 
+                       positive = "1")
+
+#ROC curve
+ggplot(kitchen.testProbs, aes(d = as.numeric(kitchen.testProbs$Outcome), m = Probs)) +
   geom_roc(n.cuts = 50, labels = FALSE, colour = "#FE9900") +
   style_roc(theme = theme_grey) +
   geom_abline(slope = 1, intercept = 0, size = 1.5, color = 'grey') +
-  labs(title = "ROC Curve - kitchen sink")
+  labs(title = "Figure 7. ROC Curve - kitchen sink")
+
+pROC::auc(kitchen.testProbs$Outcome, kitchen.testProbs$Probs)
+
+ggplot(eng.testProbs, aes(d = as.numeric(eng.testProbs$Outcome), m = Probs)) +
+  geom_roc(n.cuts = 50, labels = FALSE, colour = "#FE9900") +
+  style_roc(theme = theme_grey) +
+  geom_abline(slope = 1, intercept = 0, size = 1.5, color = 'grey') +
+  labs(title = "Figure 8. ROC Curve - engineer")
+
+pROC::auc(eng.testProbs$Outcome, eng.testProbs$Probs)
 
 #CV Goodness of Fit Metrics: kitchen sink
 dplyr::select(kitchen.cvFit$resample, -Resample) %>%
@@ -233,7 +259,7 @@ dplyr::select(kitchen.cvFit$resample, -Resample) %>%
   facet_wrap(~metric) +
   geom_vline(aes(xintercept = mean), colour = "#981FAC", linetype = 3, size = 1.5) +
   scale_x_continuous(limits = c(0, 1)) +
-  labs(x="Goodness of Fit", y="Count", title="Figure 7. CV Goodness of Fit Metrics: kitchen sink",
+  labs(x="Goodness of Fit", y="Count", title="Figure 8. CV Goodness of Fit Metrics: kitchen sink",
        subtitle = "Across-fold mean reprented as dotted lines")
 
 #CV Goodness of Fit Metrics: engineering
@@ -245,5 +271,108 @@ dplyr::select(eng.cvFit$resample, -Resample) %>%
   facet_wrap(~metric) +
   geom_vline(aes(xintercept = mean), colour = "#981FAC", linetype = 3, size = 1.5) +
   scale_x_continuous(limits = c(0, 1)) +
-  labs(x="Goodness of Fit", y="Count", title="Figure 8. CV Goodness of Fit Metrics: engineering",
+  labs(x="Goodness of Fit", y="Count", title="Figure 9. CV Goodness of Fit Metrics: engineering",
        subtitle = "Across-fold mean reprented as dotted lines")
+
+#cost benefit analysis
+#cost_benefit_table
+cost_benefit_table <-
+  eng.testProbs %>%
+  count(predOutcome, Outcome) %>%
+  summarize(True_Negative = sum(n[predOutcome==0 & Outcome==0]),
+            True_Positive = sum(n[predOutcome==1 & Outcome==1]),
+            False_Negative = sum(n[predOutcome==0 & Outcome==1]),
+            False_Positive = sum(n[predOutcome==1 & Outcome==0])) %>%
+  gather(Variable, Count) %>%
+  mutate(Revenue =
+           case_when(Variable == "True_Negative"  ~ 0,
+                     Variable == "True_Positive"  ~ ((10000-2850-5000) * (Count * .25)) + 
+                       (-2850 * (Count * .75)),
+                     Variable == "False_Negative" ~ (10000-5000) * Count,
+                     Variable == "False_Positive" ~ -2850 * Count)) %>%
+  bind_cols(data.frame(Description = c(
+    "We predicted no allocation and homeowner did not take credit",
+    "We predicted allocation and homeowner took credit",
+    "We predicted no allocation and homeowner took credit",
+    "We predicted allocation and the homeowner did not take credit")))
+
+kable(cost_benefit_table,
+      caption = "Table 1. Cost/Benefit Table",format='pipe') %>% 
+  kable_styling()
+
+#Plot the confusion metric outcomes for each Threshold
+iterateThresholds <- function(data) {
+  x = .01
+  all_prediction <- data.frame()
+  while (x <= 1) {
+    
+    this_prediction <-
+      eng.testProbs %>%
+      mutate(predOutcome = ifelse(Probs > x, 1, 0)) %>%
+      count(predOutcome, Outcome) %>%
+      summarize(True_Negative = sum(n[predOutcome==0 & Outcome==0]),
+                True_Positive = sum(n[predOutcome==1 & Outcome==1]),
+                False_Negative = sum(n[predOutcome==0 & Outcome==1]),
+                False_Positive = sum(n[predOutcome==1 & Outcome==0])) %>%
+      gather(Variable, Count) %>%
+      mutate(Revenue =
+               ifelse(Variable == "True_Negative", 0,
+                      ifelse(Variable == "True_Positive",((10000-2850-5000) * (Count * .25)) + 
+                               (-2850 * (Count * .75)),
+                             ifelse(Variable == "False_Negative", (10000-5000) * Count,
+                                    ifelse(Variable == "False_Positive", -2850 * Count, 0)))),
+             Threshold = x)
+    
+    all_prediction <- rbind(all_prediction, this_prediction)
+    x <- x + .01
+  }
+  return(all_prediction)
+}
+
+whichThreshold <- iterateThresholds(eng.testProbs)
+
+whichThreshold %>%
+  ggplot(.,aes(Threshold, Revenue, colour = Variable)) +
+  geom_point() +
+  scale_colour_manual(values = palette5[c(5, 1:3)]) +    
+  labs(title = "Figure 9. Revenue by confusion matrix type and threshold",
+       y = "Revenue") +
+  guides(colour=guide_legend(title = "Confusion Matrix")) 
+
+#Create two small multiple plots that show Threshold as a function of Total_Revenue and Total_Count_of_Credits. Interpret this.
+
+whichThreshold_revenue <- 
+  whichThreshold %>% 
+  mutate(allocations = ifelse(Variable == "True_Positive", (Count * 0.25),
+                             ifelse(Variable == "False_Negative", Count, 0))) %>% 
+  group_by(Threshold) %>% 
+  summarize(Total_Revenue = sum(Revenue),
+            Total_Count_of_Credits = sum(allocations))
+
+#Total_Count_of_Credits
+ggplot(whichThreshold_revenue)+
+  geom_line(aes(x = Threshold, y = Total_Count_of_Credits))+
+  geom_vline(xintercept =  pull(arrange(whichThreshold_revenue, -Total_Count_of_Credits)[1,1]))+
+  labs(title = "Figure 10. Total Count of Credits By Threshold For Test Sample",
+       subtitle = "Vertical Line Denotes Optimal Threshold")
+
+#Total_Revenue
+ggplot(whichThreshold_revenue)+
+  geom_line(aes(x = Threshold, y = Total_Revenue))+
+  geom_vline(xintercept =  pull(arrange(whichThreshold_revenue, -Total_Revenue)[1,1]))+
+  labs(title = "Figure 11. Model Revenues By Threshold For Test Sample",
+       subtitle = "Vertical Line Denotes Optimal Threshold")
+
+#Create a table of the Total_Revenue and Total_Count_of_Credits allocated for 2 categories. 50%_Threshold and your Optimal_Threshold.(unsolved)
+
+Threshold.compare<-
+  whichThreshold_revenue %>%
+  filter(Threshold == 0.50|
+  Threshold == 0.23) %>%
+  mutate(Model = paste(Threshold*100,"% Threshold", sep=''),
+         Total_Revenue = paste("$", Total_Revenue, spe='')) %>%
+  dplyr::select(Model, Total_Revenue, Total_Count_of_Credits)
+
+kable(Threshold.compare,
+      caption = "Table 2. the Total_Revenue and Total_Count_of_Credits allocated for 2 Threshold",format='pipe') %>% 
+  kable_styling()
